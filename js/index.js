ce792182bec6ae033fe96e530dcbcd484a78e420
Justine B <justinegayton@gmail.com>
2019-04-02 17:23:05 +0000

    Automatically backed up by Learn

--- js/index.js
+++ js/index.js
@@ -1,7 +1,7 @@
 function getRepositories() {
   const req = new XMLHttpRequest();
   req.addEventListener('load', showRepositleaories);
-  req.open('GET', 'https://api.github.com/users/:username/repos');
+  req.open('GET', 'https://api.github.com/users/octocat/repos');
   req.send();
 }
 
