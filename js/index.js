a8c043429ed858a00a22afa26f82ee39aca4fe9d
Justine B <justinegayton@gmail.com>
2019-04-02 18:19:56 +0000

    Automatically backed up by Learn

--- js/index.js
+++ js/index.js
@@ -9,9 +9,11 @@ function getRepositories() {
 function displayRepositories() {
   var repos = JSON.parse(this.responseText);
   const repoList = `<ul>${repos
-    .map(r => '<li>' 
-         +  r.name 
-         + ' - <a href="#" data-repo="' + r.name + '" onclick="getCommits(this)">Get Commits</a></li>' 
+    .map(r => '<li>' + 
+         r.name 
+         + r.html_url 
+        // + ' - <a href="#" data-repo="' + r.name + '"${repo.html_url}"> repo.html_url </a>'
+         + ' - <a href="#" data-repo="' + r.name + '" onclick="getCommits(this)">Get Commits</a>' 
          + ' - <a href="#" data-repo="' + r.name + '" onclick="getBranches(this)">Get Branches</a></li>'
         )
     .join('')}</ul>`;
