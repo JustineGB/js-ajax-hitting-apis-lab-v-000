8a6369179a189a4614e568598a26c623009eb0c6
Justine B <justinegayton@gmail.com>
2019-04-02 19:00:36 +0000

    Automatically backed up by Learn

--- js/index.js
+++ js/index.js
@@ -38,10 +38,10 @@ function getCommits(el){
 
 
 function getBranches(){}
-// function displayBranches(el) {
-//   const name = el.dataset.repo;
-//   const req = new XMLHttpRequest();
-//   req.addEventListener('load', showCommits);
-//   req.open('GET', 'https://api.github.com/repos/' + name + '/:repo/branches');
-//   req.send();
-// }
+function displayBranches(el) {
+  const name = el.dataset.repo;
+  const req = new XMLHttpRequest();
+  req.addEventListener('load', showCommits);
+  req.open('GET', 'https://api.github.com/repos/' + name + '/:repo/branches');
+  req.send();
+}
