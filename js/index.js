adfa423c41fa2e6b6978505f266b3f905293912d
Justine B <justinegayton@gmail.com>
2019-04-02 17:02:09 +0000

    Automatically backed up by Learn

--- js/index.js
+++ js/index.js
@@ -1,1 +1,22 @@
-// your code here
+function getRepositories() {
+  const req = new XMLHttpRequest();
+  req.addEventListener('load', showRepositleaories);
+  req.open('GET', 'https://api.github.com/users/:username/repos');
+  req.send();
+}
+
+function displayBranches(el) {
+  const name = el.dataset.repo;
+  const req = new XMLHttpRequest();
+  req.addEventListener('load', showCommits);
+  req.open('GET', 'https://api.github.com/repos/' + name + '/:repo/branches');
+  req.send();
+}
+
+
+
+function displayCommits() {}
+function displayRepositories() {}
+function showRepositleaories() {}
+function getCommits(){}
+function getBranches(){}
