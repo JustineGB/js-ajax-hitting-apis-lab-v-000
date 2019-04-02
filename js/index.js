46d238b4c19d8b8e6c62ede1dea32d83d29af2e6
Justine B <justinegayton@gmail.com>
2019-04-02 17:51:08 +0000

    Automatically backed up by Learn

--- js/index.js
+++ js/index.js
@@ -7,7 +7,7 @@ function getRepositories() {
 }
 
 function displayRepositories() {
-   var repos = JSON.parse(this.responseText);
+  var repos = JSON.parse(this.responseText);
   console.log(repos);
   const repoList = `<ul>${repos
     .map(r => '<li>' + r.name + '</li>')
