0291841c12a45d0724b584a724d5202482400884
Justine B <justinegayton@gmail.com>
2019-04-02 17:49:23 +0000

    Automatically backed up by Learn

--- js/index.js
+++ js/index.js
@@ -7,13 +7,15 @@ function getRepositories() {
 }
 
 function displayRepositories() {
+   var repos = JSON.parse(this.responseText);
+  console.log(repos);
     //console.log(this.responseText);
-  let repoList = '<ul>';
-  for (var i = 0; i < this.responseText.length; i++) {
-    repoList += '<li>' + this.responseText[i]['name'] + '</li>';
-  }
-  repoList += '</ul>';
-  document.getElementById('repositories').innerHTML = repoList;
+  // let repoList = '<ul>';
+  // for (var i = 0; i < this.responseText.length; i++) {
+  //   repoList += '<li>' + this.responseText[i]['name'] + '</li>';
+  // }
+  // repoList += '</ul>';
+  // document.getElementById('repositories').innerHTML = repoList;
 }
 
 function getCommits(){}
